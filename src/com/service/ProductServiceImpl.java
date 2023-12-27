8b5145329445a5ed5d0250241c0992d42db19bde
Sudershan Khadka <sudershankhadka10@gmail.com>
2023-12-27 19:04:46 +0545

    dbdemo

--- src/com/service/ProductServiceImpl.java
+++ src/com/service/ProductServiceImpl.java
@@ -2,8 +2,10 @@ package com.service;
 
 import com.model.Product;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ProductServiceImpl implements ProductService {
         static List<Product> plist = new ArrayList<>();
@@ -11,14 +13,51 @@ public class ProductServiceImpl implements ProductService {
     public void addProduct(Product prod) {
             plist.add(prod);
         System.out.println("----------Successfully Added--------"+plist.size());
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/product","admin","Password@123");
+
+            // ----> For adding  ---> sql queries
+            String sql = "insert into product (id,name,price,comapny,category) values ('"+prod.getId()+"','"+prod.getName()+"','"+prod.getPrice()+"','"+prod.getComapany()+"','"+prod.getCategory()+"')";
+
+            Statement st = con.createStatement();
+            st.execute(sql);
+            con.close();
+            System.out.println("success");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
-    @Override
+  //  @Override
     public void deleteProduct(int index) {
-        plist.remove(index);
+
+
+     //   plist.remove(index);
+
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the product id to be deleted :");
+            int prodID = s.nextInt();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/product","admin","Password@123");
+
+            // ----> For deleting   ---> sql queries
+            String sql = "delete from product where id ="+prodID;
+            Statement st = con.createStatement();
+            st.execute(sql);
+            con.close();
+            System.out.println("success");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Override
+
     public List<Product> getAllProducts() {
 
         return plist;
--- src/com/view/Test.java
+++ src/com/view/Test.java
@@ -9,53 +9,53 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        add();
-        getAll();
+//        add();
+//        getAll();
         delete();
-        getAll();    //after deleting
+//        getAll();    //after deleting
     }
     //add product
-    static void add(){
-        ProductService s = new ProductServiceImpl();
-        char flag ;
-        Scanner sc = new Scanner(System.in);
-        do {
-            Product p = new Product();
-            System.out.println("Enter id");
-            p.setId(sc.nextInt());
-
-
-            System.out.println("Enter name:");
-            p.setName(sc.next());
-
-            System.out.println("Enter category:");
-            p.setCategory(sc.next());
-
-            System.out.println("Enter comapany");
-            p.setComapany(sc.next());
-
-            System.out.println("Enter price:");
-            p.setPrice(sc.nextInt());
-
-            s.addProduct(p);
-
-            System.out.println("Do you want to add more? [y/n]");
-            flag = sc.next().charAt(0);
-        }
-        while(flag == 'y');
-    }
+//    static void add(){
+//        ProductService s = new ProductServiceImpl();
+//        char flag ;
+//        Scanner sc = new Scanner(System.in);
+//        do {
+//            Product p = new Product();
+//            System.out.println("Enter id");
+//            p.setId(sc.nextInt());
+//
+//
+//            System.out.println("Enter name:");
+//            p.setName(sc.next());
+//
+//            System.out.println("Enter category:");
+//            p.setCategory(sc.next());
+//
+//            System.out.println("Enter comapany");
+//            p.setComapany(sc.next());
+//
+//            System.out.println("Enter price:");
+//            p.setPrice(sc.nextInt());
+//
+//            s.addProduct(p);
+//
+//            System.out.println("Do you want to add more? [y/n]");
+//            flag = sc.next().charAt(0);
+//        }
+//        while(flag == 'y');
+//    }
     //get all products
 
-    static void getAll(){
-        ProductService ps = new ProductServiceImpl();
-        List<Product> plist = ps.getAllProducts();
-        System.out.println(plist);
-    }
+//    static void getAll(){
+//        ProductService ps = new ProductServiceImpl();
+//        List<Product> plist = ps.getAllProducts();
+//        System.out.println(plist);
+//    }
 
-    //delete products
+   // delete products
 
     static void delete(){
         ProductService ps = new ProductServiceImpl();
-        ps.deleteProduct(0);
+       ps.deleteProduct(2);
     }
 }
