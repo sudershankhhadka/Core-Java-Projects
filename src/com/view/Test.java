3c2bc5ad84f6e2b9805ac5b1569f3dae7542f2a7
Sudershan Khadka <sudershankhadka10@gmail.com>
2023-12-27 19:17:49 +0545

    updated

--- src/com/view/Test.java
+++ src/com/view/Test.java
@@ -9,41 +9,41 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-//        add();
+        add();
 //        getAll();
         delete();
 //        getAll();    //after deleting
     }
-    //add product
-//    static void add(){
-//        ProductService s = new ProductServiceImpl();
-//        char flag ;
-//        Scanner sc = new Scanner(System.in);
-//        do {
-//            Product p = new Product();
-//            System.out.println("Enter id");
-//            p.setId(sc.nextInt());
-//
-//
-//            System.out.println("Enter name:");
-//            p.setName(sc.next());
-//
-//            System.out.println("Enter category:");
-//            p.setCategory(sc.next());
-//
-//            System.out.println("Enter comapany");
-//            p.setComapany(sc.next());
-//
-//            System.out.println("Enter price:");
-//            p.setPrice(sc.nextInt());
-//
-//            s.addProduct(p);
-//
-//            System.out.println("Do you want to add more? [y/n]");
-//            flag = sc.next().charAt(0);
-//        }
-//        while(flag == 'y');
-//    }
+    add product
+    static void add(){
+        ProductService s = new ProductServiceImpl();
+        char flag ;
+        Scanner sc = new Scanner(System.in);
+        do {
+            Product p = new Product();
+            System.out.println("Enter id");
+            p.setId(sc.nextInt());
+
+
+            System.out.println("Enter name:");
+            p.setName(sc.next());
+
+            System.out.println("Enter category:");
+            p.setCategory(sc.next());
+
+            System.out.println("Enter comapany");
+            p.setComapany(sc.next());
+
+            System.out.println("Enter price:");
+            p.setPrice(sc.nextInt());
+
+            s.addProduct(p);
+
+            System.out.println("Do you want to add more? [y/n]");
+            flag = sc.next().charAt(0);
+        }
+        while(flag == 'y');
+    }
     //get all products
 
 //    static void getAll(){
